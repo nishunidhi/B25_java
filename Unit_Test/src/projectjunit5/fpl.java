b283e6425fd77c17773b38536987fee8d8b49463
sunilkumark11 <sunilkumark11@gmail.com>
2022-11-09 14:25:26 +0530

    first commit

---
+++ Unit_Test/src/projectjunit5/fpl.java
@@ -0,0 +1,14 @@
+package projectjunit5;
+import org.junit.jupiter.api.Test;
+public class fpl {
+	
+	@Test
+	void disp2() {
+		System.out.println("hello");
+	}
+	@Test
+	void disp3() {
+		System.out.println("hi");
+	}
+
+}
